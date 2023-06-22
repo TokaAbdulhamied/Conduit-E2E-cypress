31c03fbc5c57aa8a33199e1e73367dc4ec27609f
toka <t.abdelhamied@egyptsmartcards.com>
2023-06-22 14:56:44 +0300

    ajax requests test

--- cypress/e2e/test.spec.cy.js
+++ cypress/e2e/test.spec.cy.js
@@ -1,41 +1,65 @@
-let username, email, password;
-
-describe("Signup flow", () => {
-  it("The happy path should work", () => {
+describe("Regestration flow", () => {
+  before(() => {
     const random = Math.floor(Math.random() * 100000);
-    email = `user+${random}@realworld.io`;
-    username = `Tester${random}`;
-    password = "mysupersecretpassword";
+    cy.wrap(`Tester${random}`).as("username");
+    cy.wrap(`user+${random}@realworld.io`).as("email");
+    cy.wrap("mysupersecretpassword").as("password");
+  });
+  it("The happy path should work", function () {
+    console.log("this", this);
+
+    cy.intercept("POST", "**/api/users").as("signup-request");
     cy.visit("/register");
     cy.get(".form-control").then(($els) => {
-      cy.findByPlaceholderText("Username").type(username);
-      cy.findByPlaceholderText("Email").type(email);
-      cy.findByPlaceholderText("Password").type(password);
+      cy.findByPlaceholderText("Username").type(this.username);
+      cy.findByPlaceholderText("Email").type(this.email);
+      cy.findByPlaceholderText("Password").type(this.password);
     });
     cy.findByText("Sign up", { selector: "button" }).click();
-    cy.contains("No articles are here").should("be.visible");
-  });
-  it("check the login link", () => {
-    cy.visit("/register");
-    cy.contains("Have an account?")
-      .should("be.visible")
-      .should("have.attr", "href");
-  });
-});
 
-describe("Home page", () => {
-  it("navigate to home page", () => {
-    cy.visit("/");
-    cy.findByText("A place to share your knowledge.").should("be.visible");
-  });
-});
+    cy.wait("@signup-request").should((xhr) => {
+      let payload;
 
-describe("Login Flow", () => {
-  it("the happy path", () => {
+      // request check
+      console.log("xhr", xhr);
+      expect(xhr.request.body).to.have.property("user").and.to.be.a("object");
+      payload = xhr.request.body.user;
+      expect(payload).to.have.property("username", this.username);
+      expect(payload).to.have.property("email", this.email);
+      expect(payload).to.have.property("password", this.password);
+
+      // status check
+      expect(xhr.response.statusCode).to.equal(200);
+
+      // response check
+      expect(xhr.response.body).to.have.property("user").and.to.be.a("object");
+      payload = xhr.response.body.user;
+      expect(payload).to.have.property("username", this.username.toLowerCase());
+      expect(payload).to.have.property("email", this.email);
+      expect(payload).to.have.property("token").and.to.be.a("string").and.not.to
+        .be.empty;
+    });
+  });
+
+  // it("check the login link", function () {
+  //   cy.visit("/register");
+  //   cy.contains("Have an account?")
+  //     .should("be.visible")
+  //     .should("have.attr", "href");
+  // });
+  it("Login happy path", function () {
+    console.log("this", this);
     cy.visit("/login");
-    cy.findByPlaceholderText("Email").type(email);
-    cy.findByPlaceholderText("Password").type(password);
+    cy.findByPlaceholderText("Email").type(this.email);
+    cy.findByPlaceholderText("Password").type(this.password);
     cy.findByText("Sign in", { selector: "button" }).click();
     cy.contains("No articles are here").should("be.visible");
   });
 });
+
+// describe("Home page", () => {
+//   it("navigate to home page", () => {
+//     cy.visit("/");
+//     cy.findByText("A place to share your knowledge.").should("be.visible");
+//   });
+// });
